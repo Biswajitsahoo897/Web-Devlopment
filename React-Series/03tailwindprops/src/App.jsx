f31df3f6e148b5f9de122ed7637f75cc35bc733a
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-06-19 17:40:33 +0530

    Destructuring Props

--- React-Series/03tailwindprops/src/App.jsx
+++ React-Series/03tailwindprops/src/App.jsx
@@ -1,17 +1,23 @@
+import React from 'react';
+import './App.css';
+import './index.css';
+import Card from './components/Card';
+
+
 export default function App() {
+  // let [username, setUsername] =useState(0);
+
+  let myObj= {
+    name: 'Biswajit',
+    age: 25,
+    city: 'BBSR',
+  };
   return (
-<div class="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
-  <img class="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0" src="/image.jpg" alt=""/>
-  <div class="space-y-2 text-center sm:text-left">
-    <div class="space-y-0.5">
-      <p class="text-lg font-semibold text-black">Erin Lindford</p>
-      <p class="font-medium text-gray-500">Product Engineer</p>
-    </div>
-    <button class="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ...">
-      Message
-    </button>
-  </div>
-</div>
+  <>
+    <h2>CARD IMAGE</h2>
+    <Card username='Biswajit' mypassObj={myObj}></Card>
+    <Card username='Biswajit' btnText='Visit Site'></Card>
+  </>
   )
 }
 
