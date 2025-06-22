a006247b4571cc08b0ad7a545bb0865d9846b82e
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-06-22 14:16:41 +0530

    Final Commit , Updated App.jsx,API

--- React-Series/06CurrencyConverter/src/App.jsx
+++ React-Series/06CurrencyConverter/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [amount, setAmount] = useState(87.5); // default amount in INR
   const [from, setFrom] = useState("inr");
   const [to, setTo] = useState("usd");
-  const [convertedAmount, setConvertedAmount] = useState();
+  const [convertedAmount, setConvertedAmount] = useState(1.05); // default converted amount in USD
 
   const currInfo = useCurrencyInfo(from);
   // const currInfoTo = useCurrencyInfo(to);
@@ -18,7 +18,7 @@ function App() {
     setFrom(to);
     setTo(from);
     setConvertedAmount(amount);
-    setAmount(convertedAmount)
+    setAmount(convertedAmount);
   }
 
   const convert = () => {
@@ -27,7 +27,7 @@ function App() {
 
   return (
     <div
-      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover"
       style={{
         backgroundImage: `url('https://wallpaperaccess.com/full/1347872.jpg')`,
       }}
@@ -55,7 +55,7 @@ function App() {
             <div className="relative w-full h-0.5">
               <button
                 type="button"
-                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5 hover:bg-blue-700 transition-colors duration-300 transform"
                 onClick={swap} //why we pass swap as a reference and not call it directly? because we want to call it when the button is clicked, not when the component is rendered
               >
                 Swap
@@ -74,7 +74,8 @@ function App() {
                 amountDisabled
               />
             </div>
-            <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+            <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg
+             hover:bg-blue-700 transition-colors duration-300 transform text-md font-semibold">
               Convert {from.toUpperCase()} To {to.toUpperCase()}
             </button>
           </form>
--- React-Series/06CurrencyConverter/src/hooks/useCurrencyInfo.js
+++ React-Series/06CurrencyConverter/src/hooks/useCurrencyInfo.js
@@ -2,12 +2,16 @@ import {useState, useEffect} from "react";
 
 function useCurrencyInfo(currency) {
     const [data,setData]=useState({});
-    useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)        
-        .then((res) => res.json())
-        .then((res) => setData(res[currency]));
-        
-    }, [currency]); // only currency as dependency
+    try{
+        useEffect(()=>{
+            fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)        
+            .then((res) => res.json())
+            .then((res) => setData(res[currency]));
+
+        }, [currency]); // only currency as dependency
+    }catch(err){
+        console.log(err);
+    }
     return data;
 }
 
