2b9002540f859889fea9db4aaced9ad57a7eed91
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-07-05 17:30:34 +0530

    Added Edit Button & Some Styling

--- React-Series/11ReduxToolKitToDo/src/App.jsx
+++ React-Series/11ReduxToolKitToDo/src/App.jsx
@@ -5,10 +5,13 @@ function App() {
 
 
   return (
-    <>
-      <AddTodo/>
-      <Todos/>
-    </>
+    <div className="flex items-center justify-center bg-gradient-to-br">
+      <div className="bg-white/90 rounded-xl shadow-2xl p-8 w-full ">
+        <h1 className="text-3xl font-bold  mb-6 text-center drop-shadow-lg text-orange-600">Redux Toolkit To-Do</h1>
+        <AddTodo />
+          <Todos />
+      </div>
+    </div>
   )
 }
 
--- React-Series/11ReduxToolKitToDo/src/Features/ToDo/todoSlice.js
+++ React-Series/11ReduxToolKitToDo/src/Features/ToDo/todoSlice.js
@@ -14,17 +14,25 @@ export const todoSlice=createSlice({
                 id:nanoid() ,
                 text:action.payload.text
             }
-            state.todos.push(todo); //acessing state directly is allowed  because previous state is preserved so no need to use the Spread Operator
+            state.todos.push(todo); 
+            //acessing state directly is allowed  because previous state is preserved so no need to use the Spread Operator
         },
         // reducer 2
         removeTodo:(state,action)=>{
             state.todos=state.todos.filter((todo)=>todo.id!==action.payload)
+        },
+        // edit reducer 3
+        editTodo:(state,action)=>{
+            state.todos=state.todos.map((each)=>(
+                each.id===action.payload.id?{...each,text:action.payload.text}:each
+            ));                        
         }
     }
 })
 
-export const {addTodo,removeTodo}=todoSlice.actions //we have to export each reducer indivisualy 
+export const {addTodo,removeTodo,editTodo}=todoSlice.actions //we have to export each reducer indivisualy 
 export default todoSlice.reducer
+
 /*
 State is a current Snapshort or data for the slice 
 Action is like what Just happened? , Object describing what to do & carries data
