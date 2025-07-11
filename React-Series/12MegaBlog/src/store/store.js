b794ff476d4611fee9c903e4763b87c3addcf5bb
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-07-11 23:58:26 +0530

    Initial Commit , Added Reducer

---
+++ React-Series/12MegaBlog/src/store/store.js
@@ -0,0 +1,15 @@
+import {configureStore} from '@reduxjs/toolkit'
+
+const store=configureStore({
+    reducer:{
+
+    }
+})
+
+export default store
+
+
+/*
+A reducer is a pure function that takes the current state and an action, and returns the new state.
+
+*/
