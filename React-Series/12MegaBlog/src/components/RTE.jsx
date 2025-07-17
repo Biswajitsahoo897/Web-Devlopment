9679c1676a42851a1173c457a359bd2c783608d5
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-07-17 23:49:15 +0530

    Initial Commit for the Real Time Editor

---
+++ React-Series/12MegaBlog/src/components/RTE.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Editor } from '@tinymce/tinymce-react'
+// rich editor
+import { Controller } from 'react-hook-form'
+
+export default function RTE({ name, control, label, defaultValue = "" }) {
+    return (
+        // it is a component , props initialValue,onInit, init,apiKey
+        <div className="w-full">
+            {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+            =
+            <Controller
+                name={name || "Content"}
+                control={control}
+                // inform if some changes happen
+                render={({ field: { onChange } }) => (
+                    // the render element
+                    <Editor
+                        initialValue={defaultValue}
+                        init={{
+                            initialValue: defaultValue,
+                            height: 500,
+                            menubar: true,
+                            plugins: [
+                                "image",
+                                "advlist",
+                                "autolink",
+                                "lists",
+                                "link",
+                                "image",
+                                "charmap",
+                                "preview",
+                                "anchor",
+                                "searchreplace",
+                                "visualblocks",
+                                "code",
+                                "fullscreen",
+                                "insertdatetime",
+                                "media",
+                                "table",
+                                "code",
+                                "help",
+                                "wordcount",
+                                "anchor",
+                            ],
+                            toolbar:
+                                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+                            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+                        }}
+                        onEditorChange={onChange}
+                    />
+
+                )}
+            />
+            <input type="submit" />
+
+        </div>
+
+    )
+}
+
