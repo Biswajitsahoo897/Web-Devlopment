d72548d5d0a4367f68d819632c58438e02d4975b
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-07-17 23:48:19 +0530

    Added imports and exports

--- React-Series/12MegaBlog/src/components/index.js
+++ React-Series/12MegaBlog/src/components/index.js
@@ -3,6 +3,12 @@ import Footer from "./Footer/Footer";
 import Container from './container/Container'
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from './Signup'
+import Login from './Login'
+import PostForm from '../components/Post-Form/PostForm'
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
 
 
 export default { 
@@ -10,5 +16,11 @@ export default {
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout
 }
