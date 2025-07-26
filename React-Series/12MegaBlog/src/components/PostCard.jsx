260d1fa6160b0e2627f99e0f36e7bbc8251855d5
Biswajitsahoo897 <biswajitsahookunu2005@gmail.com>
2025-07-26 23:57:39 +0530

    Added FilePreview , Fallback image

--- React-Series/12MegaBlog/src/components/PostCard.jsx
+++ React-Series/12MegaBlog/src/components/PostCard.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import appwriteService from '../Appwrite/config.js'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-function PostCard({$id,title,featuredImage}) {
+function PostCard({ $id, title, featuredImage }) {
+  console.log("Image ID in post:", featuredImage);
   return (
     <Link to={`/post/${$id}`}>
-        <div className="w-full bg-gray-100 rounded-xl p-4 ">
-            <div className="w-full justify-center mb-4">
-                <img src={appwriteService.getFilePreview(featuredImage)} alt={title} className='rounded-xl'/>
-            </div>
-            <h2 className='text-2xl font-bold'>{title}</h2>
+      <div className="w-full bg-gray-100 rounded-xl p-4 hover:shadow-lg transition-shadow">
+        <div className="w-full flex justify-center mb-4">
+          <img
+            src={appwriteService.getFilePreview(featuredImage)}
+            alt={title}
+            onError={(e) => { e.target.src = '../assets/image.png' }}
+            className="rounded-xl max-h-64 object-cover"
+          />
         </div>
+        <h2 className="text-2xl font-bold line-clamp-2">{title}</h2>
+      </div>
     </Link>
   )
 }
